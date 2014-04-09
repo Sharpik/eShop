3711858d83fcab82c8880937375ef6579548fd53
Sharpik <hack3@seznam.cz>
2014-04-09 02:51:58 +0200

    Update EShop.java

--- src/com/LRFLEW/bukkit/eShop/EShop.java
+++ src/com/LRFLEW/bukkit/eShop/EShop.java
@@ -5,6 +5,11 @@ import java.util.HashMap;
 import org.bukkit.material.MaterialData;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.PluginManager;
+import java.util.logging.Logger;
 
 import com.LRFLEW.register.payment.Methods;
 
@@ -13,13 +18,15 @@ public class EShop extends JavaPlugin {
 	public final HashMap<MaterialData, Pricing> costs = new HashMap<MaterialData, Pricing>();
 	public final HashMap<String, MaterialData> iLocal = new HashMap<String, MaterialData>();
 	
-	private final EssentialsHook eh = new EssentialsHook();
 	private final Prefs prefs = new Prefs();
 	private final Perms perms = new Perms();
-	private final Com com = new Com(costs, iLocal, prefs, perms, eh);
+        
+        private static EShop instance;
+        static final Logger log = Logger.getLogger("Minecraft");
 
 	@Override
-	public void onDisable() {
+	public void onDisable()
+        {
 		costs.clear();
 		iLocal.clear();
 		prefs.clear();
@@ -30,24 +37,34 @@ public class EShop extends JavaPlugin {
 	}
 
 	@Override
-	public void onEnable() {
-		if (!Methods.hasMethod()) this.getServer().getPluginManager().disablePlugin(this);
-		
-		else {
-			Pricing.fillSettings(getServer().getLogger(), getDataFolder(), costs);
+	public void onEnable()
+        {
+            PluginManager pm = this.getServer().getPluginManager();
+		if (!Methods.hasMethod()) pm.disablePlugin(this);
+		else
+                {
+                        instance = this;
+                        Pricing.fillSettings(getServer().getLogger(), getDataFolder(), costs);
 			Pricing.fillILocal(getServer().getLogger(), getDataFolder(), iLocal);
 			prefs.load(getServer().getLogger(), getDataFolder());
 			perms.setYeti(prefs);
 			
-			eh.loadEssentials();
-			
 			// set our CommandExecuter for our command
-			this.getCommand("shop").setExecutor(com);
+                        //Com kommand = new Com(costs, iLocal, prefs, perms);
+                        //getCommand("shop").setExecutor(kommand);
+                        
+                        //log.info("Debug: Registruju příkaz.");
+                        getCommand("shop").setExecutor(new Com(costs, iLocal, prefs, perms));
+                        //log.info("Debug: Příkaz registrován.");
 			
-			// EXAMPLE: Custom code, here we just output some info so we can check all is well
 			PluginDescriptionFile pdfFile = getDescription();
 			System.out.println( pdfFile.getName() + " version " + pdfFile.getVersion() + " is enabled!" );
 		}
 	}
+        
+        public static EShop getInstance()
+        {
+            return instance;
+        }
 
 }
